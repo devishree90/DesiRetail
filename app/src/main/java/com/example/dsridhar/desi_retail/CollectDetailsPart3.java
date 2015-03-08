b5fc2ead06b7c38315b1069524616cde7db6848a
LalithaRajanala <lalitha.rajanala@gmail.com>
2015-03-08 23:40:07 +0530

    Changed the code to take screenshot and present a preview while the user is uploading.

--- app/src/main/java/com/example/dsridhar/desi_retail/CollectDetailsPart3.java
+++ app/src/main/java/com/example/dsridhar/desi_retail/CollectDetailsPart3.java
@@ -1,7 +1,9 @@
 package com.example.dsridhar.desi_retail;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -15,6 +17,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -27,7 +30,19 @@ import java.util.HashMap;
 
 public class CollectDetailsPart3 extends ActionBarActivity {
     static final int REQUEST_TAKE_PHOTO = 1888;
+    private static final int SELECT_PICTURE = 1;
     HashMap<String, String> prod_details;
+
+    private String selectedImagePath;
+
+    String imageFileName;
+    ImageView mImageView;
+
+    private static String root = null;
+    private static String imageFolderPath = null;
+    private String imageName = null;
+    private static Uri fileUri = null;
+    private static final int CAMERA_IMAGE_REQUEST=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,23 +58,26 @@ public class CollectDetailsPart3 extends ActionBarActivity {
         //if(REQUEST_TAKE_PHOTO && )
     }
 
-    private Intent dispatchTakePictureIntent() {
+    private Intent dispatchTakePictureIntent()
+    {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             // Create the File where the photo should go
             File photoFile = null;
-            try {
+            try
+            {
                 photoFile = createImageFile();
             } catch (IOException ex) {}
             // Continue only if the File was successfully created
-            if (photoFile != null) {
+            if (photoFile != null)
+            {
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
                         Uri.fromFile(photoFile));
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
         }
-//        galleryAddPic();
+        galleryAddPic();
         return takePictureIntent;
     }
 
@@ -68,7 +86,7 @@ public class CollectDetailsPart3 extends ActionBarActivity {
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
+        imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
@@ -117,5 +135,78 @@ public class CollectDetailsPart3 extends ActionBarActivity {
     {
         Intent intent = new Intent(this,PushProductToWebsite.class);
         startActivity(intent);
+    }
+
+    public void captureImage(View view) {
+
+        ImageView imageView = (ImageView) findViewById(R.id.photo);
+
+        // fetching the root directory
+        root = Environment.getExternalStorageDirectory().toString()
+                + "/Your_Folder";
+
+        // Creating folders for Image
+        imageFolderPath = root + "/saved_images";
+        File imagesFolder = new File(imageFolderPath);
+        imagesFolder.mkdirs();
+
+        // Generating file name
+        imageName = "test.png";
+
+        // Creating image here
+
+        File image = new File(imageFolderPath, imageName);
+
+        fileUri = Uri.fromFile(image);
+
+        imageView.setTag(imageFolderPath + File.separator + imageName);
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+        startActivityForResult(takePictureIntent,
+                CAMERA_IMAGE_REQUEST);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            switch (requestCode) {
+                case CAMERA_IMAGE_REQUEST:
+
+                    Bitmap bitmap = null;
+                    try {
+                        GetImageThumbnail getImageThumbnail = new GetImageThumbnail();
+                        bitmap = getImageThumbnail.getThumbnail(fileUri, this);
+                    } catch (FileNotFoundException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    } catch (IOException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
+
+                    // Setting image image icon on the imageview
+
+                    ImageView imageView = (ImageView) this
+                            .findViewById(R.id.photo);
+
+                    imageView.setImageBitmap(bitmap);
+
+                    break;
+
+                default:
+                    Toast.makeText(this, "Something went wrong...",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+            }
+
+        }
     }
 }
